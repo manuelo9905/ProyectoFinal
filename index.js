a5f5d1a0205670c37c4f0d1a60855674dd0fed73
Manuel_Alvarez <acmanuel5@gmail.com>
2024-09-14 13:12:00 -0600

    added POST

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const {pokemon} = require('./pokedex.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 /*
 VERBOS HTTP
-GET
-POST
-PATCH
-PUT
-DELETE
+GET - obtener recursos
+POST - almacenar/crear registros
+PUT - modificar un recurso
+PATCH - modificar una parte de un recurso
+DELETE - borrar un recurso
 */
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get("/pokemon/all", (req,res,next) => {
-    res.status(200);
-    res.send(pokemon);
+app.post("/pokemon", (req, res, next) => {
+    return res.status(200).send(req.body);
+})
+
+app.get("/pokemon", (req,res,next) => {
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
     const id = req.params.id - 1;
     if(id >= 0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]);
+        return res.status(200).send(pokemon[req.params.id - 1]);
     }
    else{
-    res.status(404);
-    res.send("Pokémon no encontrado");
+    return res.status(404).send("Pokémon no encontrado");
    }
 });
 
-app.get('/pokemon/:name', (req, res, next)=> {
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next)=> {
     const name =req.params.name;
-    for(i=0; i<pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
+
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+
+    });
+
+    if(pk.length > 0) {
+        res.status(200).send(pk);
     }
-    res.status(404);
-    res.send("Pokémon no encontrado");
-})
+    
+    return res.status(404).send("Pokémon no encontrado");
+    });
+
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running...");
