12110d9e3214157739dec222d94aeba79043083a
Manuel_Alvarez <acmanuel5@gmail.com>
2024-10-04 13:49:06 -0600

    ok

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,7 +3,7 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 pokemon.post("/", async (req, res, next) => {
-    const { pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
 
     if(pok_name && pok_height && pok_weight && pok_base_experience){
         let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
@@ -20,6 +20,21 @@ pokemon.post("/", async (req, res, next) => {
      return res.status(500).json({code: 500, message: "Campos incompletos"});
     
 })
+
+pokemon.delete("/:id([0-9]{1,3})", async(req, res, next) =>{
+    const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+
+    const rows  = await db.query(query);
+
+    if(rows.affectedRows == 1){
+        return res.status(200).json({code: 200, message: "Pokemon borrado correctamente" });
+    }
+    return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
+});
+
+pokemon.put("/:id([0-9]{1,3})", async(req, res, next) =>{
+    
+});
 
 pokemon.get("/", async (req,res,next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
