068ae1aaab45af3cbf8b1426195eb4f883572d8d
Manuel_Alvarez <acmanuel5@gmail.com>
2024-10-24 12:54:45 -0600

    Nuevo

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.use("/pokemon", pokemon);
 
 app.use(notFound);
 
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT || 5000, () =>{
     console.log("Server is running...");
 });
